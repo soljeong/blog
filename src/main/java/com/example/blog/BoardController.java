c2c07a278e417b8e4d705d4f4b972959118ce5d0
jeongsol <jeongsol@outlook.com>
2023-12-08 17:42:27 +0900

    보드 엔터티 리스트 표시

---
+++ src/main/java/com/example/blog/BoardController.java
@@ -0,0 +1,30 @@
+package com.example.blog;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Controller
+public class BoardController {
+
+    private final BoardService boardService;
+
+
+    @GetMapping("/board/list")
+    public String list(Model model) {
+        List<Board> boardList = this.boardService.getList();
+        model.addAttribute("boardList", boardList);
+        return "board_list";
+
+    }
+
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/board/list";
+    }
+}
--- src/test/java/com/example/blog/BlogApplicationTests.java
+++ src/test/java/com/example/blog/BlogApplicationTests.java
@@ -1,13 +1,23 @@
 package com.example.blog;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import java.time.LocalDateTime;
 
 @SpringBootTest
 class BlogApplicationTests {
 
+	@Autowired
+	BoardRepository boardRepository;
+
 	@Test
 	void contextLoads() {
+		Board b1 = new Board();
+		b1.setTitle("title222");
+		b1.setContent("cone");
+		b1.setCreateDate(LocalDateTime.now());
+		boardRepository.save(b1);
 	}
 
 }
